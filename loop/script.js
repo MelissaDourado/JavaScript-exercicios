0abfb328e1735db8609fb072ccdf512ef98526f0
Melissa Dourado da Costa <melissadouradodacosta096@gmail.com>
2024-11-07 21:06:04 -0300

    mexi com listas

---
+++ loop/script.js
@@ -0,0 +1,35 @@
+//for (let i = 0; i< 10; i ++) {
+  //  console.log (i);
+//}
+
+//let a = 0
+//while (a <5) {
+  //  console.log (a)
+//a = a+1
+//}
+
+//let frutas = [ "jabuticaba", "pitanga", "seriguela", "mamão", "abacate" ]
+//console.log (frutas.length)
+//for (let contador =0;contador < frutas.length; contador++) {
+  //  console.log (frutas [contador])
+//}
+
+// let inverso = [];
+// let frutas = ["jabuticaba", "pitanga", "seriguela", "mamão", "abacate"]
+// for (let contador=0; contador < frutas.length; contador++ ){
+//     inverso.unshift (frutas [contador]);
+//     console.log (inverso)
+// }
+
+// let num= 9
+// for (a=0; a<11; a++){
+// console.log (num*a)
+// }
+
+
+let lista1= [8,75,1, 35, 64, 45, 33, 67,]
+let pares=[]
+for (index=0;index<lista1.length;index++){
+if (lista1[index])
+}
+console.log (pares);
