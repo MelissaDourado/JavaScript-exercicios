a85331d8ec82873f1ec26e069a4435b40e9a6e8b
Melissa Dourado da Costa <melissadouradodacosta096@gmail.com>
2024-11-13 20:57:27 -0300

    aprendi sobre condições e listas

---
+++ exercicios/SomaLista.js
@@ -0,0 +1,7 @@
+//exemplo de array de numeros
+let lista = [4,9,6,5,42, 70, 136518]
+let total= 0
+for (let m=0; m<lista.length; m++){
+total = total + lista [m]
+}
+console.log (total)
---
+++ exercicios/media.js
@@ -0,0 +1,12 @@
+function media(n1, n2, n3,n4){
+let mediacalculada = (n1+ n2+ n3+ n4)/4
+media = (media + (2 + 4 + 1 + 0/4))
+if (mediacalculada<7){
+    console.log ("reprovado")
+}else{
+    console.log ("aprovado")
+}
+
+    }
+    media (6,2,1,3)
+
